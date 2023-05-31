897d7df56c3070ba1db8dba40861beedffe08d97
Emma Ericsson <emma@krlsg.se>
2023-05-31 09:08:59 +0200

    koha integration

---
+++ src/config_template.php
@@ -0,0 +1,6 @@
+<?php
+// vim sw=2 ts=2 et
+return array(
+  "auth" => "AUTH_TOKEN",
+);
+?>
--- src/submit.php
+++ src/submit.php
@@ -1,12 +1,29 @@
 <!-- Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php -->
 
 <?php
+$config = include("./config.php");
 
+$params = array(
+    "barcode" => $_POST["barcode"],
+    "borrower" => $_POST["borrower"],
+    "branch" => "SPETS",
+);
+
+$opts = array(
+    CURLOPT_URL => "https://koha-staff.solsiden.xyz/cgi-bin/koha/circ/circulation.pl",
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => $params,
+    CURLOPT_COOKIE => "CGISESSID=" . $config["auth"],
+);
+
+$ch = curl_init();
+curl_setopt_array($ch, $opts);
+curl_exec($ch);
+curl_close($ch);
 ?>
 
 <html>
     <body>
-        <h1>hejsan</h1>
-        <p><?php echo $_POST["barcode"]; ?></p>
+        <p>Successfully lent out <?php echo $_POST["barcode"]; ?> to <?php echo $_POST["borrower"]; ?></p>
     </body>
 </html>
